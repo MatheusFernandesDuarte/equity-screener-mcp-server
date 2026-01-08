e3ec19a5cce3e548f3d76718384c5f744019d138
Matheus <matheus@creditosc.com.br>
2026-01-07 22:31:57 -0300

    feat(config): initialize selenium and application settings

---
+++ src/config/config.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.chrome.options import Options
+
+
+class AppConfig:
+    BASE_URL = "https://finance.yahoo.com/research-hub/screener/equity/?start=0&count=100"
+
+    @staticmethod
+    def get_selenium_options():
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        )
+        return options
